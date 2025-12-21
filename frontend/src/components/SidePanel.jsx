030704411aba45fc201d71fe2e9e6c702bd23983
raj2237 <rajveershah7111@gmail.com>
2025-12-21 16:05:19 +0530

    Frontend updates for interactive side panel

--- frontend/src/components/SidePanel.jsx
+++ frontend/src/components/SidePanel.jsx
@@ -2,13 +2,90 @@ import React from "react";
 import KnowledgeGraph from "./KnowledgeGraph";
 import ThinkingSteps from "./ThinkingSteps";
 import RetrievalDetails from "./RetrievalDetails";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 export default function SidePanel({ graphData, currentThinkingSteps, messages }) {
     return (
         <div className="side-panel">
-            <KnowledgeGraph graphData={graphData} />
+            {/**GRAPH DATA POPUP */}
+            <Popup
+                trigger={
+                    <div className="clickable-content">
+                        <KnowledgeGraph graphData={graphData} />
+                    </div>
+                }
+                modal
+                nested
+            >
+                {
+                    (close) => (
+                        <div className="popup-box">
+                            <div className="popup-header">
+                                <h2>Graph Data</h2>
+                                <button onClick={close}>X</button>
+                            </div>
+
+                            <KnowledgeGraph graphData={graphData} />
+
+                        </div>
+                    )
+                }
+            </Popup>
+
+            {/** Thinking Steps Popup */}
+            <Popup
+                trigger={
+                    <div className="clickable-content">
+                        <ThinkingSteps currentThinkingSteps={currentThinkingSteps} />
+                    </div>
+                }
+                modal
+                nested
+            >
+                {
+                    (close) => (
+                        <div className="popup-box">
+                            <div className="popup-header">
+                                <h2>thinking steps</h2>
+                                <button onClick={close}>X</button>
+                            </div>
+
+                            <ThinkingSteps currentThinkingSteps={currentThinkingSteps} />
+
+                        </div>
+                    )
+                }
+            </Popup>
+
+            {/** Retrieval Details Popup */}
+            <Popup
+                trigger={
+                    <div className="clickable-content">
+                        <RetrievalDetails messages={messages} />
+                    </div>
+                }
+                modal
+                nested
+            >
+                {
+                    (close) => (
+                        <div className="popup-box">
+                            <div className="popup-header">
+                                <h2>thinking steps</h2>
+                                <button onClick={close}>X</button>
+                            </div>
+
+                            <RetrievalDetails messages={messages} />
+
+                        </div>
+                    )
+                }
+            </Popup>
+
+            {/* <KnowledgeGraph graphData={graphData} /> 
             <ThinkingSteps currentThinkingSteps={currentThinkingSteps} />
-            <RetrievalDetails messages={messages} />
+            <RetrievalDetails messages={messages} /> */}
         </div>
     );
 }
